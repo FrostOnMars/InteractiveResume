2d74a5c1e54dee4763c68d2e815b2c27abd73db0
Jonathan Martin <jonmartin1990@gmail.com>
2023-09-26 13:26:21 -0400

    Added some buttons, demonstrated the bindings using Observable Objects and Observable properties that are bound from the View to the View Model. Created folders for the MVVM subtypes and moved the MainWindow into the View Folder for better code organization.

---
+++ InteractiveResume/View Model/MainWindowViewModel.cs
@@ -0,0 +1,42 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace InteractiveResume.View_Model;
+
+public partial class MainWindowViewModel : ObservableObject
+{
+    [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(ClickCommand))]
+    [NotifyPropertyChangedFor(nameof(FullName))]
+    private string? firstName = "Mikayla";
+
+    [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(ClickCommand))]
+    [NotifyPropertyChangedFor(nameof(FullName))]
+    private string? lastName = "Baltierra";
+
+    [ObservableProperty] 
+    private string? fullName;
+
+
+    public MainWindowViewModel()
+    {
+        FullName = $"{firstName} {lastName}";
+    }
+
+    [RelayCommand(CanExecute = nameof(CanClick))]
+    public void Click()
+    {
+        FirstName = "Jon";
+        LastName = "Martin";
+        FullName = $"{FirstName} {LastName}";
+    }
+
+    private bool CanClick => FirstName == "Mikayla" && LastName == "Baltierra";
+}
---
+++ InteractiveResume/View/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System.Windows;
+using InteractiveResume.View_Model;
+
+namespace InteractiveResume.View
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MainWindowViewModel viewModel => DataContext as MainWindowViewModel;
+        public MainWindow()
+        {
+            DataContext = new MainWindowViewModel();
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Access the ActualHeight and ActualWidth properties of the window to automatically set the grid size
+            MainWindowGrid.Height= ActualHeight;
+            MainWindowGrid.Width = ActualWidth;
+        }
+
+        private void Button1_OnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button2_OnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button3_OnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button4_OnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button5_OnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button6_OnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Button7_OnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
