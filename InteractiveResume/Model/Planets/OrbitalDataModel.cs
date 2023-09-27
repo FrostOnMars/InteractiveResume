9a1fb2f7fe832b030acec857da8349f7eb0bca63
Jonathan Martin <jonmartin1990@gmail.com>
2023-09-27 13:40:31 -0400

    Added orbital data model.

---
+++ InteractiveResume/Model/Planets/OrbitalDataModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InteractiveResume.Model.Planets;
+
+[Serializable]
+public class OrbitalDataModel
+{
+    public string SemimajorAxis { get; set; }
+    public string Perihelion { get; set; } 
+    public string Aphelion { get; set; } 
+    public string Eccentricity { get; set; }
+    public string OrbitalPeriod { get; set; } 
+    public string OrbitalSpeed { get; set; }
+}
