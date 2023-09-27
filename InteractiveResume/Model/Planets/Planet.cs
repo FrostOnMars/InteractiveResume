473e18517f30eacf0582522e3b91260abd9179e6
Jonathan Martin <jonmartin1990@gmail.com>
2023-09-27 11:51:59 -0400

    Setting up a galaxy

---
+++ InteractiveResume/Model/Planets/Planet.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InteractiveResume.Model.Planets
+{
+    public class Planet
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<string> OrbitalData { get; set; }
+        public double Velocity { get; set; }
+
+        private Planet() { }
+
+        public static Planet Terraform(string name)
+        {
+            return new Planet
+            {
+                Name = name
+            };
+        }
+    }
+}
---
+++ InteractiveResume/View Model/NASA/OrbitalData.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InteractiveResume.View_Model.NASA
+{
+    public class OrbitalData
+    {
+        public async Task<JObject> GetPlanetDataAsync(string planetName)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetStringAsync($"https://api.le-systeme-solaire.net/rest/bodies/{planetName}");
+                return JObject.Parse(response);
+            }
+        }
+    }
+}
